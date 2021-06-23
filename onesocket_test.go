57a787dc81bbc6b61b09935571229250e0e4075c
nigel <nkmatyukira@outlook.com>
2021-06-23 19:02:11 +0200

    first commit

---
+++ onesocket_test.go
@@ -0,0 +1,11 @@
+package onesocket
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	slice := []string{"1", "2", "3"}
+	want := []string{"1", "3"}
+	if got := RemoveIndex(slice, 1); got[1] != want[1] {
+		t.Errorf("RemoveIndex() = %q, want %q", got, want)
+	}
+}
